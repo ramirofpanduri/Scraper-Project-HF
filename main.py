d74bd2bd07aab9f7e9da573425fd4ae3c32de04d
Ramiro <ramirofpanduri@gmail.com>
2025-04-28 14:39:02 -0300

    Final Scraper

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
+from dotenv import load_dotenv
+import os
 import httpx
 from bs4 import BeautifulSoup
+import mysql.connector
+
+load_dotenv()
+
+db_host = os.getenv("DB_HOST")
+db_user = os.getenv("DB_USER")
+db_password = os.getenv("DB_PASSWORD")
+db_database = os.getenv("DB_DATABASE")
+
+conn = mysql.connector.connect(
+    host=db_host,
+    user=db_user,
+    password=db_password,
+    database=db_database
+)
+
+cursor = conn.cursor()
 
 url = "https://www.houseoffraser.co.uk/men/hoodies-and-sweatshirts"
 headers = {
@@ -15,13 +34,20 @@ def extract_product_data(product):
         price = product.find(
             "span", class_="CurrencySizeLarge curprice").text.strip()
         print(f"brand: {brand} Name: {name} Price: {price}")
+        guardar_producto(brand, name, price)
     except Exception as e:
         print(e)
 
 
+def guardar_producto(brand, name, price):
+    sql = "INSERT INTO products (brand, name, price) VALUES (%s, %s, %s)"
+    val = (brand, name, price)
+    cursor.execute(sql, val)
+    conn.commit()
+
+
 def main():
     response = httpx.get(url, headers=headers)
-    print(response.text)
     response_html = response.text
 
     soup = BeautifulSoup(response_html, "html.parser")
@@ -32,3 +58,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+cursor.close()
+conn.close()
