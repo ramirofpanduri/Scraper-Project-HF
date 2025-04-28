d2f6c12e7f523ca67e9dd019eddbf9852cd8b4d6
Ramiro <ramirofpanduri@gmail.com>
2025-04-28 13:05:35 -0300

    Finished Scraper

--- main.py
+++ main.py
@@ -8,12 +8,26 @@ headers = {
 }
 
 
+def extract_product_data(product):
+    try:
+        name = product.find("span", class_="productdescriptionname").text
+        brand = product.find("span", class_="productdescriptionbrand").text
+        price = product.find(
+            "span", class_="CurrencySizeLarge curprice").text.strip()
+        print(f"brand: {brand} Name: {name} Price: {price}")
+    except Exception as e:
+        print(e)
+
+
 def main():
     response = httpx.get(url, headers=headers)
     print(response.text)
     response_html = response.text
 
     soup = BeautifulSoup(response_html, "html.parser")
+    products = soup.find_all("div", class_="s-productthumbbox")
+    for product in products:
+        extract_product_data(product)
 
 
 if __name__ == "__main__":
